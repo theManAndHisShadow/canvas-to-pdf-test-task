1e60304b0c7b4e3a2b9b015065fa56dfd77ebc96
theManAndHisShadow <justemailpostbox@gmail.com>
2024-12-22 19:34:09 +0500

    [Main]: The main code of the application has been updated to include the new scene system.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,15 @@
 import { debugLog, decimalToRGB, getColor } from "./helpers";
-import createStripedContainer from "./core/stripedContainer";
+
 import * as PIXI from 'pixi.js-legacy';
 import UI from "./UI/UI";
 
 // Импортируем тему и вспомогательную функциз из библиотеки 'json2html'
 import draculaV2Theme from "./libs/json2html/themes/user.theme";
 import json2html from "./libs/json2html/json2html.min";
+
+// импортируем подготовленные сцены
+import createShowcaseScene from "./scenes/showcase.scene";
+import createCompositionScene from "./scenes/composition.scene";
 
 // локальная хелпер функция для отрисовки объекта в html
 const convertObjectToHTML = (objectToRender: object) => {
@@ -40,6 +44,7 @@ const app = new PIXI.Application({
     width: 500,
     height: 500,
     background: getColor('carbon'),
+    forceCanvas: true,
 });
 
 // Добавляем в HTML дерево холст
@@ -64,157 +69,82 @@ const mainContainerUpdateHandler = () => {
 mainContainer.on('childAdded', mainContainerUpdateHandler);
 mainContainer.on('childRemoved', mainContainerUpdateHandler);
 
-// очистка содержимого главного контейнера
-ui.elements.clearCanvas.addEventListener('click', () => {
-    mainContainer.removeChildren();
+// Массив подготовленных сцен
+// Имя ключа совпадает с клюбчом из списка 'ui.element.selectedScenes.valuesList'
+const scenes: Record<string, any> = {
+    showcase: createShowcaseScene({
+        centerPoint: {x: width / 2, y: height / 2}
+    }),
 
-    debugLog('info', '`ui.elements.clearCanvas` click event triggered');
-});
-
-
-
-// Далее отрисовка элементов холста
-interface CollectionItem {
-    zIndex: number;
-    objectRef: any;
-    renderAt: (container: PIXI.Container) => PIXI.Graphics | PIXI.Container | PIXI.Sprite;
-}
-
-const objects: Record<string, CollectionItem> = {
-    triangle_1: {
-        zIndex: 1,
-        objectRef: null,
-        renderAt: (container: PIXI.Container) => {
-            // Создаём заготовку фигуры
-            const graphics = new PIXI.Graphics();
-            
-            // Отрисовываем заготовку как зелёный треугольник
-            graphics.beginFill(getColor('darkGreen'))
-            graphics.lineStyle(3, getColor('brightGreen'))
-            graphics.drawPolygon([
-                    168, 50,   // верхняя вершина
-                    288, 250,  // правая нижняя вершина
-                    55, 250    // левая нижняя вершина
-            ]);
-
-            // graphics.endFill();
-
-            container.addChild(graphics);
-            return graphics;
-        },
-    },
-
-    circle_1: {
-        zIndex: 3,
-        objectRef: null,
-        renderAt: (container: PIXI.Container) => {
-            // Создаём заготовку фигуры
-            const graphics = new PIXI.Graphics();
-
-            // Отрисовываем заготовку как красный круг
-            graphics.lineStyle(3, getColor('brightRed'))
-            graphics.beginFill(getColor('darkRed'));
-            graphics.drawCircle(centerPos.x, centerPos.y, 100);
-
-            // graphics.endFill();
-
-            container.addChild(graphics);
-            return graphics;
-        },
-    },
-
-    square_1: {
-        zIndex: 4,
-        objectRef: null,
-        renderAt: (container: PIXI.Container) => {
-            // Создаём заготовку фигуры
-            const graphics = new PIXI.Graphics();
-
-            graphics.lineStyle(3, getColor('brightBlue'));
-            graphics.beginFill(getColor('darkBlue'));
-            graphics.drawRect(centerPos.x, centerPos.y, 170, 170);
-            // graphics.endFill();
-            
-            container.addChild(graphics);
-            return graphics;
-        },
-    },
-
-    subContainer_1: {
-        zIndex: 1,
-        objectRef: null,
-        renderAt: (container: PIXI.Container) => {
-            // Создаём контейнер с полосками, внутри содержатся суб-элементы в виде линий
-            const subContainer_1 = createStripedContainer(53, 256, 193, 165, 2, 5, 'purple');
-
-            container.addChild(subContainer_1);
-            return subContainer_1;
-        },
-    }, 
-    
-    sprite_1: {
-        zIndex: 0,
-        objectRef: null,
-        renderAt: (container: PIXI.Container) => {
-            const windowTexture = PIXI.Texture.from('../assets/window.png');
-            const windowSprite = new PIXI.Sprite(windowTexture);
-
-            let coeff = 0.75;
-            windowSprite.width = 339 * coeff;
-            windowSprite.height = 262 * coeff;
-
-            windowSprite.x = 167;
-            windowSprite.y = 48;
-
-            container.addChild(windowSprite);
-            return windowSprite;
-        },
-    },
+    composition: createCompositionScene({
+        centerPoint: {x: width / 2, y: height / 2},
+    }),
 };
 
-const renderQueue = Object.fromEntries(Object.entries(objects).sort(([, a], [, b]) => a.zIndex - b.zIndex));
+// 
+let selectedName = JSON.parse(localStorage.getItem('selectedScene'));
+let selectedScene: PIXI.Container = scenes[selectedName];
 
-// Отрсовываем объекты и добавляем интерактивность
-for(const key in renderQueue) {
-    let object = objects[key];
-    let renderedObject = object.renderAt(mainContainer);
+// Инициализация ранее выбранной сцены
+mainContainer.addChild(selectedScene);
 
-    renderedObject.eventMode = 'dynamic';
-    renderedObject.cursor = 'pointer';
 
-    renderedObject.on('pointerover', () => {
-        let data: any = {
-            name: key,
-        };
+// Добавляем интерактивность к объектам всех сцен
+Object.values(scenes).forEach(scene => {
+    for (const object of scene.children) {
+        object.eventMode = 'dynamic';
+        object.cursor = 'pointer';
 
-        if(renderedObject instanceof PIXI.Sprite) {
-            data["class"] = "PIXI.Sprite";
-            data.texture = {
-                url: renderedObject._texture.textureCacheIds[0].split('/')[2],
-                width: renderedObject._texture.width,
-                height: renderedObject._texture.height,
+        // Событие при наведении
+        object.on('pointerover', () => {
+            let data: any = {
+                name: object.label,
+            };
+
+            // Настраиваем объект data в зависимости от типа объекта сцены
+            if (object instanceof PIXI.Sprite) {
+                data["class"] = "PIXI.Sprite";
+                data.texture = {
+                    url: object._texture.textureCacheIds[0].split('/')[2],
+                    width: object._texture.width,
+                    height: object._texture.height,
+                }
+            } else if (object instanceof PIXI.Graphics) {
+                data["class"] = "PIXI.Graphics";
+                // так конечно лучше не делать, но в данном случае я получаю цвет, а не назначаю
+                data.fillColor = getColor(decimalToRGB((object as any)._fillStyle.color));
+                data.lineColor = getColor(decimalToRGB((object as any)._lineStyle.color));
+            } else if (object instanceof PIXI.Container) {
+                data["class"] = "PIXI.Container";
+                data.contains = object.children.length + ' elements';
             }
-        } else if(renderedObject instanceof PIXI.Graphics) {
-            data["class"] = "PIXI.Graphics";
-            // так конечно лучше не делать, но в данном случае я получаю цвет, а не назначаю
-            data.fillColor = getColor(decimalToRGB((renderedObject as any)._fillStyle.color));
-            data.lineColor = getColor(decimalToRGB((renderedObject as any)._lineStyle.color));
-        } else if(renderedObject instanceof PIXI.Container) {
-            data["class"] = "PIXI.Container";
-            data.contains = renderedObject.children.length + ' elements';
-        }
 
-        ui.elements.mouseTarget.replaceChild(convertObjectToHTML(data));
+            // Обновляем данные по текущей цели мыши
+            ui.elements.mouseTarget.replaceChild(convertObjectToHTML(data));
 
-        debugLog('info', '`'+ key +'` pointerover event triggered');
-    });
+            debugLog('info', '`' + object.label + '` pointerover event triggered');
+        });
 
-    renderedObject.on('pointerout', () => {
-        ui.elements.mouseTarget.resetValue();
-    });
+        object.on('pointerout', () => {
+            ui.elements.mouseTarget.resetValue();
+        });
+    }
+});
 
-    object.objectRef = renderedObject;
-}
+// Добавляем возможность выбрать сцену из списка доступных
+ui.elements.selectedScene.addEventListener('change', () => {
+    // Очищаем сцену от предыдущих элементов
+    mainContainer.removeChildren();
+
+    // Получаем имя сцены из localStorage и находим в массиве сцен
+    selectedName = JSON.parse(localStorage.getItem('selectedScene'));
+    selectedScene = scenes[selectedName]; 
+
+    // передобавляем сцену в главный окнтейнер
+    mainContainer.addChild(selectedScene);
+
+    debugLog('ok', 'scene successfully changed');
+});
  
 // Добавляем контейнер на уровень (холст)
 app.stage.addChild(mainContainer);
