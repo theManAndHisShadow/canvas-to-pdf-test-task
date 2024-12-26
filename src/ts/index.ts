e198e25f6dadebe0577d3c8d29ecdcaef083a8c5
theManAndHisShadow <justemailpostbox@gmail.com>
2024-12-26 12:41:24 +0500

    [Global]: project's main file totally rewrited.
    Changelog:
    - now project uses draft version of PIXI to SKIA convertation (by using 'convertPixiContainerToSkia()');
    - now app starts only after SKIA canvaskit module initialization end (wrapped ti canvaskitInit promise);
    - added two separate canvas for PIXI and SKIA init;
    - added several 'debugLog()' calls.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,4 @@
-import { debugLog, decimalToRGB, getColor } from "./helpers";
+import { debugLog, decimal2RGBString, getColor } from "./helpers";
 
 import * as PIXI from 'pixi.js-legacy';
 import UI from "./UI/UI";
@@ -11,6 +11,9 @@ import json2html from "./libs/json2html/json2html.min";
 import createShowcaseScene from "./scenes/showcase.scene";
 import createCompositionScene from "./scenes/composition.scene";
 
+import convertPixiContainerToSkia from "./core/convert";
+import CanvasKitInit from "canvaskit-wasm";
+
 // локальная хелпер функция для отрисовки объекта в html
 const convertObjectToHTML = (objectToRender: object) => {
     //@ts-ignore
@@ -22,134 +25,172 @@ const convertObjectToHTML = (objectToRender: object) => {
     });
 };
 
-// Глобальная ширина и высота холста
-const width = 500;
-const height = 500;
+CanvasKitInit({ locateFile: (file) => `../js/${file}` }).then((canvasKit) => {
+    debugLog("ok", "`canvaskit.wasm` successfully loaded to project");
+    debugLog("ok", "`CanvasKit` instance is accessible within the given scope");
 
-const appRootSelector = '#app-root';
+    // Глобальная ширина и высота холста
+    const width = 450;
+    const height = 450;
 
-// Корневой элемент приложения
-const appRoot = document.querySelector(appRootSelector);
+    const appRootSelector = '#app-root';
 
-// Проверяем что корень существует и выкидываем ошибку если корень ен найден
-if(!appRoot) {
-    debugLog('err', "can`t find '#app-root' element at DOM");
-}
+    // Корневой элемент приложения
+    const appRoot = document.querySelector(appRootSelector);
 
-// Центр холста
-const canvasCenterPoint = {x: width / 2, y: height / 2};
-
-// Инициалищзируем pixi приложение
-const app = new PIXI.Application({
-    width: 500,
-    height: 500,
-    background: getColor('carbon'),
-    forceCanvas: true,
-});
-
-// Добавляем в HTML дерево холст
-appRoot.appendChild(app.view as HTMLCanvasElement);
-
-// создлаём объект интерфейса
-const ui = new UI(appRootSelector, 350, height);
-
-// Создаём главный контейнер
-const mainContainer = new PIXI.Container();
-
-// Обработчик события "обновление контейнера"
-const mainContainerUpdateHandler = () => {
-    if(mainContainer.children.length > 0) {
-        const stageData = {
-            contains: `${mainContainer.children[0].children.length} elements`,
-        };
-    
-        ui.elements.mainContainerInfo.replaceChild(convertObjectToHTML(stageData));
+    // Проверяем что корень существует и выкидываем ошибку если корень ен найден
+    if (!appRoot) {
+        debugLog('err', "can`t find '#app-root' element at DOM");
     }
-};
 
-// обновляем инфо при обновлении наполнения контейнера
-mainContainer.on('childAdded', mainContainerUpdateHandler);
-mainContainer.on('childRemoved', mainContainerUpdateHandler);
+    const pixijsCanvas: HTMLCanvasElement = document.querySelector('#app__pixijs-canvas');
+    pixijsCanvas.width = width;
+    pixijsCanvas.height = height;
 
-// Массив подготовленных сцен
-// Имя ключа совпадает с ключом из списка 'ui.element.selectedScenes.valuesList'
-const scenes: Record<string, any> = {
-    showcase: createShowcaseScene({
-        centerPoint: canvasCenterPoint,
-    }),
+    const skiaCanvas: HTMLCanvasElement = document.querySelector('#app__skia-canvas');
+    skiaCanvas.width = width;
+    skiaCanvas.height = height;
 
-    composition: createCompositionScene({
-        centerPoint: canvasCenterPoint,
-    }),
-};
+    // Центр холста
+    const canvasCenterPoint = { x: width / 2, y: height / 2 };
 
-// 
-let selectedName = JSON.parse(localStorage.getItem('selectedScene'));
-let selectedScene: PIXI.Container = scenes[selectedName];
+    // Инициализируем pixi приложение
+    const app = new PIXI.Application({
+        forceCanvas: true,
+        view: pixijsCanvas,
+        width: width,
+        height: height,
+        background: getColor('carbon'),
+    });
 
-// Инициализация ранее выбранной сцены
-mainContainer.addChild(selectedScene);
+    // Добавляем в HTML дерево холст
+    appRoot.appendChild(app.view as HTMLCanvasElement);
 
+    // создлаём объект интерфейса
+    const ui = new UI(appRootSelector, 350, height);
 
-// Добавляем интерактивность к объектам всех сцен
-Object.values(scenes).forEach(scene => {
-    for (const object of scene.children) {
-        object.eventMode = 'dynamic';
-        object.cursor = 'pointer';
+    // Создаём главный контейнер
+    const mainContainer = new PIXI.Container();
 
-        // Событие при наведении
-        object.on('pointerover', () => {
-            let data: any = {
-                name: object.label,
+    // Обработчик события "обновление контейнера"
+    const mainContainerUpdateHandler = () => {
+        if (mainContainer.children.length > 0) {
+            const stageData = {
+                contains: `${mainContainer.children[0].children.length} elements`,
             };
 
-            // Настраиваем объект data в зависимости от типа объекта сцены
-            if (object instanceof PIXI.Sprite) {
-                data["class"] = "PIXI.Sprite";
-                data.texture = {
-                    url: object._texture.textureCacheIds[0].split('/')[2],
-                    width: object._texture.width,
-                    height: object._texture.height,
-                }
-            } else if (object instanceof PIXI.Graphics) {
-                data["class"] = "PIXI.Graphics";
-                // так конечно лучше не делать, но в данном случае я получаю цвет, а не назначаю
-                data.fillColor = getColor(decimalToRGB((object as any)._fillStyle.color));
-                data.lineColor = getColor(decimalToRGB((object as any)._lineStyle.color));
-            } else if (object instanceof PIXI.Container) {
-                data["class"] = "PIXI.Container";
-                data.contains = object.children.length + ' elements';
-            }
+            ui.elements.mainContainerInfo.replaceChild(convertObjectToHTML(stageData));
+        }
+    };
 
-            // Обновляем данные по текущей цели мыши
-            ui.elements.mouseTarget.replaceChild(convertObjectToHTML(data));
+    // обновляем инфо при обновлении наполнения контейнера
+    mainContainer.on('childAdded', mainContainerUpdateHandler);
+    mainContainer.on('childRemoved', mainContainerUpdateHandler);
 
-            debugLog('info', '`' + object.label + '` pointerover event triggered');
-        });
+    // Массив подготовленных сцен
+    // Имя ключа совпадает с ключом из списка 'ui.element.selectedScenes.valuesList'
+    const scenes: Record<string, any> = {
+        showcase: createShowcaseScene({
+            centerPoint: canvasCenterPoint,
+        }),
 
-        object.on('pointerout', () => {
-            ui.elements.mouseTarget.resetValue();
-        });
-    }
-});
+        composition: createCompositionScene({
+            centerPoint: canvasCenterPoint,
+        }),
+    };
 
-// Добавляем возможность выбрать сцену из списка доступных
-ui.elements.selectedScene.addEventListener('change', () => {
-    // Очищаем сцену от предыдущих элементов
-    mainContainer.removeChildren();
+    // 
+    let selectedName = JSON.parse(localStorage.getItem('selectedScene'));
+    let selectedScene: PIXI.Container = scenes[selectedName];
 
-    // Получаем имя сцены из localStorage и находим в массиве сцен
-    selectedName = JSON.parse(localStorage.getItem('selectedScene'));
-    selectedScene = scenes[selectedName]; 
-
-    // передобавляем сцену в главный окнтейнер
+    // Инициализация ранее выбранной сцены
     mainContainer.addChild(selectedScene);
 
-    debugLog('ok', 'scene successfully changed');
+    // convertPixiContainerToSkia({
+    //     from: mainContainer,
+    //     to: skiaCanvas,
+    // }).catch(console.error);
+
+
+    // Добавляем интерактивность к объектам всех сцен
+    Object.values(scenes).forEach(scene => {
+        for (const object of scene.children) {
+            object.eventMode = 'dynamic';
+            object.cursor = 'pointer';
+
+            // Событие при наведении
+            object.on('pointerover', () => {
+                let data: any = {
+                    name: object.label,
+                };
+
+                // Настраиваем объект data в зависимости от типа объекта сцены
+                if (object instanceof PIXI.Sprite) {
+                    data["class"] = "PIXI.Sprite";
+                    data.texture = {
+                        url: object._texture.textureCacheIds[0].split('/')[2],
+                        width: object._texture.width,
+                        height: object._texture.height,
+                    }
+
+                } else if (object instanceof PIXI.Graphics) {
+                    data["class"] = "PIXI.Graphics";
+                    // так конечно лучше не делать, но в данном случае я получаю цвет, а не назначаю
+                    data.fillColor = getColor(decimal2RGBString((object as any)._fillStyle.color));
+                    data.lineColor = getColor(decimal2RGBString((object as any)._lineStyle.color));
+                } else if (object instanceof PIXI.Container) {
+                    data["class"] = "PIXI.Container";
+                    data.contains = object.children.length + ' elements';
+                }
+
+                // Обновляем данные по текущей цели мыши
+                ui.elements.mouseTarget.replaceChild(convertObjectToHTML(data));
+
+                debugLog('info', '`' + object.label + '` pointerover event triggered');
+            });
+
+            object.on('pointerout', () => {
+                ui.elements.mouseTarget.resetValue();
+            });
+        }
+    });
+
+
+    // Добавляем возможность выбрать сцену из списка доступных
+    ui.elements.selectedScene.addEventListener('change', () => {
+        // Очищаем сцену от предыдущих элементов
+        mainContainer.removeChildren();
+
+        // Получаем имя сцены из localStorage и находим в массиве сцен
+        selectedName = JSON.parse(localStorage.getItem('selectedScene'));
+        selectedScene = scenes[selectedName];
+
+        // передобавляем сцену в главный окнтейнер
+        mainContainer.addChild(selectedScene);
+
+
+        // После успешной загрузки используйте canvasKit
+        convertPixiContainerToSkia({
+            from: mainContainer,
+            to: skiaCanvas,
+            use: canvasKit,
+        }).catch(console.error);
+
+        debugLog('ok', 'scene successfully changed');
+    });
+
+    // Добавляем контейнер на уровень (холст)
+    app.stage.addChild(mainContainer);
+
+
+    // После успешной загрузки используйте canvasKit
+    convertPixiContainerToSkia({
+        from: mainContainer,
+        to: skiaCanvas,
+        use: canvasKit,
+    }).catch(console.error);
+
+    debugLog('ok', 'the application is fully launched and ready to work');
+}).catch((error) => {
+    console.error('Ошибка загрузки CanvasKit:', error);
 });
- 
-// Добавляем контейнер на уровень (холст)
-app.stage.addChild(mainContainer);
-
-
-debugLog('ok', 'app started');
