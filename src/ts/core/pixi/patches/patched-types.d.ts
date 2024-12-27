aa735665964eff4c3a324bc976155e0b57b401c9
theManAndHisShadow <justemailpostbox@gmail.com>
2024-12-27 13:22:01 +0500

    [Core]: added new patches.

--- src/ts/core/pixi/patches/patched-types.d.ts
+++ src/ts/core/pixi/patches/patched-types.d.ts
@@ -1,7 +1,15 @@
-import { Graphics } from "pixi.js-legacy";
+import { Graphics, Container, DisplayObject } from "pixi.js-legacy";
 
 declare module "pixi.js-legacy" {
     interface Graphics {
+        label: string | null;
+    }
+
+    interface Container {
+        label: string | null;
+    }
+
+    interface DisplayObject {
         label: string | null;
     }
 }
