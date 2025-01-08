bfa548f77faa119e414c8caeae95cc0e7ca077a4
theManAndHisShadow <justemailpostbox@gmail.com>
2025-01-08 18:23:25 +0500

    [Core]: updated 'drawPolygon()', function now supports 'isCloesed' property.

--- src/ts/core/pixi2skia/shapes.ts
+++ src/ts/core/pixi2skia/shapes.ts
@@ -58,30 +58,32 @@ export function drawCircle(canvas: Canvas, circle: PIXI.Circle, styles: any, can
 
 
 export function drawPolygon(canvas: Canvas, points: number[], styles: any, canvasKit: CanvasKit) {
-    const fillPaint = new canvasKit.Paint();
-    fillPaint.setStyle(canvasKit.PaintStyle.Fill);
-    fillPaint.setAntiAlias(true);
-    fillPaint.setColor(canvasKit.Color(...decimal2RGBA(styles.fillColor)));
-
     const path = new canvasKit.Path();
     path.moveTo(points[0], points[1]);
 
     for (let i = 2; i < points.length; i += 2) {
         path.lineTo(points[i], points[i + 1]);
     }
-    path.close();
 
-    // Рисуем заливку
-    canvas.drawPath(path, fillPaint);
+    if(styles.isClosed) path.close();
+
+    // Рисуем заливку, только если задан цвет заливки
+    if (styles.fillColor) {
+        const fillPaint = new canvasKit.Paint();
+        fillPaint.setStyle(canvasKit.PaintStyle.Fill);
+        fillPaint.setAntiAlias(true);
+        fillPaint.setColor(canvasKit.Color(...decimal2RGBA(styles.fillColor)));
+        canvas.drawPath(path, fillPaint);
+    }
 
     // Рисуем обводку
-    if(styles.lineWidth > 0) {
+    if (styles.lineWidth > 0) {
         const strokePaint = new canvasKit.Paint();
         strokePaint.setStyle(canvasKit.PaintStyle.Stroke);
         strokePaint.setAntiAlias(true);
         strokePaint.setColor(canvasKit.Color(...decimal2RGBA(styles.lineColor)));
         strokePaint.setStrokeWidth(styles.lineWidth);
-        
+
         canvas.drawPath(path, strokePaint);
     }
 }
