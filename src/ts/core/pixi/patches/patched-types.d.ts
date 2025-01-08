407e50a8487fe628993d76669253ed239888c88b
theManAndHisShadow <justemailpostbox@gmail.com>
2025-01-08 18:09:28 +0500

    [Core]: added 'isClosed' property-patch to 'Graphics' class.

--- src/ts/core/pixi/patches/patched-types.d.ts
+++ src/ts/core/pixi/patches/patched-types.d.ts
@@ -2,14 +2,28 @@ import { Graphics, Container, DisplayObject } from "pixi.js-legacy";
 
 declare module "pixi.js-legacy" {
     interface Graphics {
+        /**
+         * Graphics object string label
+         */
         label: string | null;
+
+        /**
+         * Marks if graphics shape is not closed
+         */
+        isClosed: boolean;
     }
 
     interface Container {
+        /**
+         * Container object string label
+         */
         label: string | null;
     }
 
     interface DisplayObject {
+        /**
+         * DisplayObject string label
+         */
         label: string | null;
     }
 }
